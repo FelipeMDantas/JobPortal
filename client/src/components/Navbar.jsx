52aef101db57c0eaac69d5d4fa899e1994e45e7d
FelipeMDantas <felipemdantas@terra.com.br>
2025-01-19 15:28:08 -0300

    [feat] Configure sign-in mechanism

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,27 @@
+import { useClerk, useUser } from "@clerk/clerk-react";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
         <img src={assets.logo} alt="" />
-        <div className="flex gap-4 max-sm:text-xs">
-          <button className="text-gray-600">Recruiter Login</button>
-          <button className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">
-            Login
-          </button>
-        </div>
+        {user ? (
+          <dib></dib>
+        ) : (
+          <div className="flex gap-4 max-sm:text-xs">
+            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={(e) => openSignIn()}
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+            >
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
