05e5851d7135a7b4e803e2a93b36e5aaa0002480
FelipeMDantas <felipemdantas@terra.com.br>
2025-01-20 22:02:44 -0300

    [feat] Add categories checkbox filter

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,20 +1,23 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { assets } from "../assets/assets";
+import { assets, JobCategories } from "../assets/assets";
 
 const JobListing = () => {
   const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
 
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
-      <div>
+      <div className="w-full lg:w-1/4 bg-white px-4">
         {isSearched &&
           (searchFilter.title !== "" || searchFilter.location !== "") && (
             <>
-              <h3>Current Search</h3>
-              <div>
+              <h3 className="font-medium text-lg mb-4">Current Search</h3>
+              <div className="mb-4 text-gray-600">
                 {searchFilter.title && (
-                  <span>
+                  <span
+                    className="inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 
+                  py-1.5 rounded"
+                  >
                     {searchFilter.title}
                     <img
                       onClick={(e) =>
@@ -27,7 +30,10 @@ const JobListing = () => {
                   </span>
                 )}
                 {searchFilter.location && (
-                  <span>
+                  <span
+                    className="inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 
+                  py-1.5 rounded"
+                  >
                     {searchFilter.location}
                     <img
                       onClick={(e) =>
@@ -42,6 +48,18 @@ const JobListing = () => {
               </div>
             </>
           )}
+
+        <div>
+          <h4>Search by Categories</h4>
+          <ul>
+            {JobCategories.map((category, index) => (
+              <li key={index}>
+                <input type="checkbox" name="" id="" />
+                {category}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
