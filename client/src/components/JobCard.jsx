efd033f760b8d4a8f148ded98a0ffe5bc3c49ab0
FelipeMDantas <felipemdantas@terra.com.br>
2025-01-21 22:44:10 -0300

    [feat] Further develop the JobCard component

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -3,14 +3,26 @@ import { assets } from "../assets/assets";
 
 const JobCard = ({ job }) => {
   return (
-    <div>
-      <div>
-        <img src={assets.company_icon} alt="" />
+    <div className="border p-6 shadow rounded">
+      <div className="flex justify-between items-center">
+        <img className="h-8" src={assets.company_icon} alt="" />
       </div>
-      <h4>
-        <span>{job.location}</span>
-        <span>{job.level}</span>
-      </h4>
+      <h4 className="font-medium text-xl mt-2">{job.title}</h4>
+      <div className="flex items-center gap-3 mt-2 text-xs">
+        <span className="bg-blue-50 border border-blue-200 px-4 py-1.5 rounded">
+          {job.location}
+        </span>
+        <span className="bg-red-50 border border-red-200 px-4 py-1.5 rounded">
+          {job.level}
+        </span>
+      </div>
+      <p className="text-gray-500 text-sm mt-4">
+        {job.description.slice(0, 150).replace("<p>", "")}...
+      </p>
+      <div className="mt-4 flex gap-4 text-sm">
+        <button>Apply now</button>
+        <button>Learn more</button>
+      </div>
     </div>
   );
 };
