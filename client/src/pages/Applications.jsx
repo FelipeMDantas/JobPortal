0f900d1d0c1290d8d98d052b5f05af07f2d175b9
FelipeMDantas <felipemdantas@terra.com.br>
2025-01-28 23:37:03 -0300

    [feat] Configure the save button and define the "Jobs Applied" table

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -14,8 +14,10 @@ const Applications = () => {
         <div className="flex gap-2 mb-6 mt-3">
           {isEdit ? (
             <>
-              <label htmlFor="resumeUpload">
-                <p>Select Resume</p>
+              <label className="flex items-center" htmlFor="resumeUpload">
+                <p className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2">
+                  Select Resume
+                </p>
                 <input
                   id="resumeUpload"
                   onChange={(e) => setResume(e.target.files[0])}
@@ -25,7 +27,12 @@ const Applications = () => {
                 />
                 <img src={assets.profile_upload_icon} alt="" />
               </label>
-              <button>Save</button>
+              <button
+                onClick={(e) => setIsEdit(false)}
+                className="bg-green-100 border border-green-400 rounded-lg px-4 py-2"
+              >
+                Save
+              </button>
             </>
           ) : (
             <div className="flex gap-2">
@@ -44,6 +51,18 @@ const Applications = () => {
             </div>
           )}
         </div>
+        <h2>Jobs Applied</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Job Title</th>
+              <th>Location</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </>
   );
