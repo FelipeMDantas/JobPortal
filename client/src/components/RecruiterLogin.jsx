4ed6f43d67e406f69cb0adcf624c4db7c815fbae
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-02 23:19:42 -0300

    [feat] Style the RecruiterLogin component

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -10,25 +10,34 @@ const RecruiterLogin = () => {
   const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
 
   return (
-    <div>
-      <form>
-        <h1>Recruiter {state}</h1>
-        <p>Welcome back! Please sign in to continue</p>
+    <div
+      className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center 
+    items-center"
+    >
+      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          Recruiter {state}
+        </h1>
+        <p className="text-sm">Welcome back! Please sign in to continue</p>
         <>
-          <div>
-            <img src={assets.person_icon} alt="" />
-            <input
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              type="text"
-              placeholder="Company Name"
-              required
-            />
-          </div>
+          {state !== "Login" && (
+            <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+              <img src={assets.person_icon} alt="" />
+              <input
+                className="outline-none text-sm"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                placeholder="Company Name"
+                required
+              />
+            </div>
+          )}
 
-          <div>
-            <img src={assets.lock_icon} alt="" />
+          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+            <img src={assets.email_icon} alt="" />
             <input
+              className="outline-none text-sm"
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               type="email"
@@ -37,9 +46,10 @@ const RecruiterLogin = () => {
             />
           </div>
 
-          <div>
-            <img src={assets.person_icon} alt="" />
+          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+            <img src={assets.lock_icon} alt="" />
             <input
+              className="outline-none text-sm"
               onChange={(e) => setPassword(e.target.value)}
               value={password}
               type="password"
@@ -47,9 +57,15 @@ const RecruiterLogin = () => {
               required
             />
           </div>
+
+          <p className="text-sm text-blue-600 my-4 cursor-pointer">
+            Forgot your password?
+          </p>
         </>
 
-        <button>{state === "Login" ? "Login" : "Create Account"}</button>
+        <button className="bg-blue-600 w-full text-white py-2 rounded-full">
+          {state === "Login" ? "Login" : "Create Account"}
+        </button>
       </form>
     </div>
   );
