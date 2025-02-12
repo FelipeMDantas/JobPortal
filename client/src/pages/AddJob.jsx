4c1060115440dd435a1c81e7919ef0c470bdd0e0
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-11 23:50:59 -0300

    [feat] Build a rich text editor in the AddJob page.

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Quill from "quill";
 
 const AddJob = () => {
   const [title, setTitle] = useState("");
@@ -6,6 +7,17 @@ const AddJob = () => {
   const [category, setCategory] = useState("Programming");
   const [level, setLevel] = useState("Senior level");
   const [salary, setSalary] = useState(0);
+
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
 
   return (
     <form>
@@ -18,6 +30,18 @@ const AddJob = () => {
           value={title}
           required
         />
+      </div>
+
+      <div>
+        <p>Job Description</p>
+        <div ref={editorRef}></div>
+      </div>
+
+      <div>
+        <div>
+          <p>Job Category</p>
+          <select onChange={(e) => setCategory(e.target.value)}></select>
+        </div>
       </div>
     </form>
   );
