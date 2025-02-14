84c8c751209b5b64dffb3116945271cdf5505c3f
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-14 15:40:18 -0300

    [feat] Structure the ViewApplications page table

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,5 +1,35 @@
+import { viewApplicationsPageData } from "../assets/assets";
+
 const ViewApplications = () => {
-  return <div>ViewApplications</div>;
+  return (
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <ht>#</ht>
+              <ht>User Name</ht>
+              <ht>Job Title</ht>
+              <ht>Location</ht>
+              <ht>Resume</ht>
+              <ht>Action</ht>
+            </tr>
+          </thead>
+          <tbody>
+            {viewApplicationsPageData.map((applicant, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>
+                  <img src={applicant.imgSrc} alt="" />
+                  <span>{applicant.name}</span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ViewApplications;
