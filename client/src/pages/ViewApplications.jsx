5d5bc95d4239f2311bac8adc8b727f2b6296d2e9
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-15 15:59:15 -0300

    [feat] Style the ViewApplications page table

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,27 +1,47 @@
-import { viewApplicationsPageData } from "../assets/assets";
+import { assets, viewApplicationsPageData } from "../assets/assets";
 
 const ViewApplications = () => {
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <div>
-        <table>
+        <table className="w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm">
           <thead>
-            <tr>
-              <ht>#</ht>
-              <ht>User Name</ht>
-              <ht>Job Title</ht>
-              <ht>Location</ht>
-              <ht>Resume</ht>
-              <ht>Action</ht>
+            <tr className="border-b">
+              <th className="py-2 px-4 text-left">#</th>
+              <th className="py-2 px-4 text-left">User Name</th>
+              <th className="py-2 px-4 text-left max-sm:hidden">Job Title</th>
+              <th className="py-2 px-4 text-left max-sm:hidden">Location</th>
+              <th className="py-2 px-4 text-left">Resume</th>
+              <th className="py-2 px-4 text-left">Action</th>
             </tr>
           </thead>
           <tbody>
             {viewApplicationsPageData.map((applicant, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>
-                  <img src={applicant.imgSrc} alt="" />
+              <tr key={index} className="text-gray-700">
+                <td className="py-2 px-4 border-b text-center">{index + 1}</td>
+                <td className="py-2 px-4 border-b text-center flex">
+                  <img
+                    className="w-10 h-10 rounded-full mr-3 max-sm:hidden"
+                    src={applicant.imgSrc}
+                    alt=""
+                  />
                   <span>{applicant.name}</span>
+                </td>
+                <td>{applicant.jobTitle}</td>
+                <td>{applicant.location}</td>
+                <td>
+                  <a href="" target="_blank">
+                    Resume <img src={assets.resume_download_icon} alt="" />
+                  </a>
+                </td>
+                <td>
+                  <div>
+                    <button>...</button>
+                    <div>
+                      <button>Accept</button>
+                      <button>Reject</button>
+                    </div>
+                  </div>
                 </td>
               </tr>
             ))}
