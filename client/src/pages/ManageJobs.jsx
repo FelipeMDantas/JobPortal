086e4beaebded6cea7a8b9bc89b65d012e963359
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-16 22:37:01 -0300

    [feat] Structure the ManageJobs page table

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,5 +1,22 @@
 const ManageJobs = () => {
-  return <div>ManageJobs</div>;
+  return (
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Job Title</th>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Applicants</th>
+              <th>Visible</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageJobs;
