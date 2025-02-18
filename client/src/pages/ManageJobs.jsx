456f141350071ff43bd2d3b7d4d26b354ebb5b21
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-17 22:53:45 -0300

    [feat] Fill the table by iterating over the jobs data

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,18 +1,41 @@
+import moment from "moment";
+import { manageJobsData } from "../assets/assets";
+
 const ManageJobs = () => {
   return (
-    <div>
-      <div>
-        <table>
+    <div className="container p-4 max-w-5xl">
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-200 max-sm:text-sm">
           <thead>
             <tr>
-              <th>#</th>
-              <th>Job Title</th>
-              <th>Date</th>
-              <th>Location</th>
-              <th>Applicants</th>
-              <th>Visible</th>
+              <th className="py-2 px-4 border-b text-left max-sm:hidden">#</th>
+              <th className="py-2 px-4 border-b text-left">Job Title</th>
+              <th className="py-2 px-4 border-b text-left max-sm:hidden">
+                Date
+              </th>
+              <th className="py-2 px-4 border-b text-left max-sm:hidden">
+                Location
+              </th>
+              <th className="py-2 px-4 border-b text-center">Applicants</th>
+              <th className="py-2 px-4 border-b text-left">
+                Visible
+              </th>
             </tr>
           </thead>
+          <tbody>
+            {manageJobsData.map((job, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{job.title}</td>
+                <td>{moment(job.data).format("ll")}</td>
+                <td>{job.location}</td>
+                <td>{job.applicants}</td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
