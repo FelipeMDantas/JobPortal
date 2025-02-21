a88150ec1f0fbaa9b22a1d88605895d04e344c3b
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-20 22:52:17 -0300

    [feat] Establish server port and initialize it

--- server/server.js
+++ server/server.js
@@ -6,3 +6,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => res.send("API Working"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
