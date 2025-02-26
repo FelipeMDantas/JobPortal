6db3f66532dcdc8e7686082869dbd3dfbbef8803
FelipeMDantas <felipemdantas@terra.com.br>
2025-02-25 23:28:43 -0300

    [feat] Configure the clerk webhook headers

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,15 @@
+import { Webhook } from "svix";
+
+export const clerkWebHooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+
+    const { data, type } = req.body;
+  } catch (error) {}
+};
