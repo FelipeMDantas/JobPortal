08757f2ef37d547acfb98234113ccd87f3ca5176
FelipeMDantas <felipemdantas@terra.com.br>
2025-03-01 20:23:06 -0300

    [feat] Define company controller functions

---
+++ server/controllers/companyController.js
@@ -0,0 +1,6 @@
+export const registerCompany = async (req, res) => {};
+export const loginCompany = async (req, res) => {};
+export const getCompanyData = async (req, res) => {};
+export const postJob = async (req, res) => {};
+export const getCompanyJobApplicants = async (req, res) => {};
+export const getCompanyPostedJobs = async (req, res) => {};
