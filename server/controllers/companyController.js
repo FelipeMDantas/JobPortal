dee7974125f153b3661070f944c36ddf3bbef75d
FelipeMDantas <felipemdantas@terra.com.br>
2025-03-07 23:18:15 -0300

    [feat] Code a JWT token generation function

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,6 @@
 import Company from "../models/Company.js";
 import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
 
 export const registerCompany = async (req, res) => {
   const { name, email, password } = req.body;
@@ -22,7 +23,24 @@ export const registerCompany = async (req, res) => {
 
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
-    
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path);
+
+    const company = await Company.create({
+      name,
+      email,
+      password: hashPassword,
+      image: imageUpload.secure_url,
+    });
+
+    res.json({
+      success: true,
+      company: {
+        _id: company._id,
+        name: company.name,
+        email: company.email,
+        image: company.image,
+      },
+    });
   } catch (error) {}
 };
 
