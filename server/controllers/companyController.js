8ea9fe77e885287750ff30871aca59f2b5cee893
FelipeMDantas <felipemdantas@terra.com.br>
2025-03-13 22:35:17 -0300

    [feat] Code the postJob function

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -2,6 +2,7 @@ import Company from "../models/Company.js";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
+import Job from "../models/Job.js";
 
 export const registerCompany = async (req, res) => {
   const { name, email, password } = req.body;
@@ -73,7 +74,30 @@ export const loginCompany = async (req, res) => {
   }
 };
 export const getCompanyData = async (req, res) => {};
-export const postJob = async (req, res) => {};
+export const postJob = async (req, res) => {
+  const { title, description, location, salary, level, category } = req.body;
+
+  const companyId = req.company._id;
+
+  try {
+    const newJob = new Job({
+      title,
+      description,
+      location,
+      salary,
+      companyId,
+      date: Date.now(),
+      level,
+      category,
+    });
+
+    await newJob.save();
+
+    res.json({ success: true, newJob });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 export const getCompanyJobApplicants = async (req, res) => {};
 export const getCompanyPostedJobs = async (req, res) => {};
 export const changeHobApplicationsStatus = async (req, res) => {};
