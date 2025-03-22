f63af665e235af15d7fcb1d40c74ef6d11b5528e
FelipeMDantas <felipemdantas@terra.com.br>
2025-03-21 22:35:48 -0300

    [build] Install the @clerk/express SDK

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import companyRoutes from "./routes/companyRoutes.js";
 import connectToCloudinary from "./config/cloudinary.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
@@ -17,6 +18,8 @@ await connectToCloudinary();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
+
 app.use("/api/company", companyRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/users", userRoutes);
