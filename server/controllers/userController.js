5058d8d3972ea8e81650b57937898b50746cd25b
FelipeMDantas <felipemdantas@terra.com.br>
2025-03-22 22:47:44 -0300

    [feat] Define the JobApplication data model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,20 @@
-export const getUserData = async (req, res) => {};
+import User from "../models/User.js";
+
+export const getUserData = async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User Not Found" });
+    }
+
+    res.json({ success: true, user });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export const applyForJob = async (req, res) => {};
 
