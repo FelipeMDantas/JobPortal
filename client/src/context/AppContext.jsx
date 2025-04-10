fd51ed5d2384696ac64affca81f7576e501a98bf
FelipeMDantas <felipemdantas@terra.com.br>
2025-04-09 23:13:19 -0300

    [feat] Code the user data fetch function

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,15 @@
 import { createContext, useEffect, useState } from "react";
-import { jobsData } from "../assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const { user } = useUser();
+  const { getToken } = useAuth();
 
   const [searchFilter, setSearchFilter] = useState({ title: "", location: "" });
   const [isSearched, setIsSearched] = useState(false);
@@ -14,6 +17,8 @@ export const AppContextProvider = (props) => {
   const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
   const [companyToken, setCompanyToken] = useState(null);
   const [companyData, setCompanyData] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [userApplications, setUserApplications] = useState([]);
 
   const fetchJobs = async () => {
     try {
@@ -27,7 +32,6 @@ export const AppContextProvider = (props) => {
     } catch (error) {
       toast.error(error.message);
     }
-    setJobs(jobsData);
   };
 
   const fetchCompanyData = async () => {
@@ -38,6 +42,24 @@ export const AppContextProvider = (props) => {
 
       if (data.success) {
         setCompanyData(data.company);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const fetchUserData = async () => {
+    try {
+      const token = await getToken();
+
+      const { data } = await axios.get(backendUrl + "/api/users/user", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setUserData(data.user);
       } else {
         toast.error(data.message);
       }
