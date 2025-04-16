f4bb906f7a73c5a4eb2cf41ac83cf00d9bcfa201
FelipeMDantas <felipemdantas@terra.com.br>
2025-04-15 22:24:48 -0300

    [feat] Code the fetchUserApplications funtion

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -68,6 +68,24 @@ export const AppContextProvider = (props) => {
     }
   };
 
+  const fetchUserApplications = async () => {
+    try {
+      const token = await getToken();
+
+      const { data } = await axios.get(backendUrl + "/api/users/applications", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setUserApplications(data.applications);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     fetchJobs();
 
@@ -87,6 +105,7 @@ export const AppContextProvider = (props) => {
   useEffect(() => {
     if (user) {
       fetchUserData();
+      fetchUserApplications();
     }
   }, [user]);
 
@@ -109,6 +128,7 @@ export const AppContextProvider = (props) => {
     userApplications,
     setUserApplications,
     fetchUserData,
+    fetchUserApplications,
   };
 
   return (
